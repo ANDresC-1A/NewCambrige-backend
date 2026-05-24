84badb6ffc4b2b1b93d0ae34db3cb3f2f2663e2b
AndreIng16 <andresuarez0816@gmail.com>
2026-05-24 14:23:03 -0500

    Update schemas.py

--- app/modules/salon/schemas.py
+++ app/modules/salon/schemas.py
@@ -1,6 +1,32 @@
 from pydantic import BaseModel
 from typing import Optional
 from datetime import date
+# ======================
+# 🏫 SALONES
+# ======================
+class SalonBase(BaseModel):
+    grado: int
+    grupo: int
+    id_usuario: Optional[int] = None
+    id_periodo: Optional[int] = None
+
+
+class SalonCreate(SalonBase):
+    pass
+
+
+class SalonUpdate(BaseModel):
+    grado: Optional[int] = None
+    grupo: Optional[int] = None
+    id_usuario: Optional[int] = None
+    id_periodo: Optional[int] = None
+
+
+class SalonResponse(SalonBase):
+    id_salon: int
+
+    class Config:
+        from_attributes = True
 
 # ======================
 # 🧪 PRUEBAS

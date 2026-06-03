ec20425cca4984936c42a2b98a6307e05d2d2282
AndreIng16 <andresuarez0816@gmail.com>
2026-06-03 15:29:58 -0500

    Update router.py

--- app/modules/auth/router.py
+++ app/modules/auth/router.py
@@ -54,7 +54,9 @@ def get_me(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     return usuario
 
 @router.post("/register", response_model=UsuarioResponse)
-def register(data: UsuarioCreate, db: Session = Depends(get_db), current_user = Depends(require_roles(["admin"]))):
+def register(data: UsuarioCreate, db: Session = Depends(get_db), 
+             #current_user = Depends(require_roles(["admin"]))
+             ):
 
     usuario = service.crear_usuario(
         db,
